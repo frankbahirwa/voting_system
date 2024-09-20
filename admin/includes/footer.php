54b70abca01ce5bd1cfff1aa4483a14be5d533da
frank bahirwa <frankbahirwa@gmail.com>
2024-09-20 13:10:13 +0200

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Voting System</b>
+    </div>
+    <strong> &copy; Brought To You By Frank - Bahirwa L5sod 2024-2025</strong>
+</footer>
